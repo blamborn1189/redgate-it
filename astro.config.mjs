fdd5a3db7c39b95a7846f73ed147b95aae56af28
BigB <227877260+blamborn1189@users.noreply.github.com>
2026-02-11 17:15:03 -0500

    Configure for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://blamborn1189.github.io',
+  base: '/redgate-it',
+  output: 'static'
+});
